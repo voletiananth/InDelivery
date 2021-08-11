dc365ee92bc2b414cc5f0bfa707c3b83ab3577fa
Voleti Ananth Kumar <voletiananthkumar@gmail.com>
2021-08-11 21:04:59 +0530

    Intial Setup

---
+++ app/src/main/java/com/example/indelivery/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.indelivery
+
+import android.os.Bundle
+import com.google.android.material.appbar.CollapsingToolbarLayout
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import com.example.indelivery.databinding.ActivityScrollingBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityScrollingBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityScrollingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+    }
+}
